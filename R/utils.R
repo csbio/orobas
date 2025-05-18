5b38085e0a1d408f5bd7e07e1c12041ad20e9810
ArshiaZHassan <92499152+ArshiaZHassan@users.noreply.github.com>
2025-05-18 18:06:55 -0500

    Update utils.R add function format_replicate_names()

--- R/utils.R
+++ R/utils.R
@@ -308,3 +308,11 @@ abbreviate_names <- function(df, pattern, indices) {
   colnames(df)[indices] <- abbrev_cols
   return(df)
 }
+
+# Fixes replicate names
+format_replicate_names <- function(rep_names) {
+  rep_names <- as.character(trimws(rep_names, which = "right"))
+  fixes <- c("-_" = "", "_-" = "", "-" = "", ", " = "", " \\+ " = "", " " = "")
+  rep_names <- str_replace_all(rep_names, fixes)
+  return(rep_names)
+}
